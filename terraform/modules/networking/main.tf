1b44ac6950a2efa171f6a1c074d0874230a2de9c
Samuel Braga <samuel.fantini.braga@hotmail.com>
2020-08-17 16:25:32 -0300

    Adicionando regra de seguranca para a nsg

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -16,6 +16,30 @@ resource "azurerm_network_security_group" "netwoking_security_group" {
     name                = var.nsg_name
     location            = var.resource_group_location
     resource_group_name = var.resource_group_name
+
+    security_rule {
+        name                       = "Port_80"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "Port_443"
+        priority                   = 110
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 }
 
 resource "azurerm_subnet_network_security_group_association" "associete_nsg_subnet" {
